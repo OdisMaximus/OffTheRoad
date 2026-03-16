fa22636b3dee0a9ce4171c60e364543da9f4391b
ipato2 <ipato2@uic.edu>
2026-03-15 22:18:35 -0500

    Added back functionality of shade to building, also added functionality of 'highlighting' via pressing Spacebar so player always has access on what to do next with objects

---
+++ Assets/ProjectAssets/Scripts/HighlightBehavior.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject theModel;
+    public MeshRenderer modelRenderer;
+    public BoxCollider theModelBoxCollider;
+
+    public Color startColor = new Color(14, 219, 230, 255); //PLACEHOLDER DEFAULT
+    public Color endColor = new Color(14, 219, 230, 255); //PLACEHOLDER DEFAULT
+
+    public Color currentColor = Color.red;
+
+    public float time = 1.0f;
+    public float speed = 1.0f;
+
+    public bool allowHighlight = true;
+
+    public GameObject shadeGrabbableObject;
+    public GameObject newGameManagerObject;
+    public NewGameManager newGameManagerScript;
+
+    void Start()
+    {
+        modelRenderer = theModel.GetComponent<MeshRenderer>();
+        theModelBoxCollider = theModel.GetComponent<BoxCollider>();
+        newGameManagerScript = newGameManagerObject.GetComponent<NewGameManager>();
+        //InvokeRepeating("CycleColor", 2.0f, 2.0f);
+    }
+
+    void HighlightShadeToPlace()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            modelRenderer.enabled = true;
+            modelRenderer.material.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time * speed, 1));
+        }
+        else
+        {
+            modelRenderer.enabled = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowHighlight) 
+        {
+            HighlightShadeToPlace();
+        }
+    }
+
+    //IF SHADE IS PLACED, TURN ON SHADE
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("ShadeTag"))
+        {
+            allowHighlight = false;
+            modelRenderer.enabled = true;
+            modelRenderer.material.color = currentColor;
+            Destroy(shadeGrabbableObject);
+
+            
+
+            //ADD TO 'shadeScore'
+            newGameManagerScript.NumShadePlacedUpdate();
+
+        }
+    }
+
+
+    /*void CycleColor()
+    {
+        towardsColor = !towardsColor;
+        if (towardsColor)
+        {
+            
+        } 
+        else
+        {
+            currentColor = Color.Lerp(endColor, startColor, time);
+        }
+
+        modelRenderer.material.color = currentColor;
+    }*/
+}
---
+++ Assets/ProjectAssets/Scripts/NewGameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int numShadePlaced = 0;
+
+    public GameObject CongestionObjectsWest;
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(numShadePlaced == 6)
+        {
+            OpenInteraction3();
+            numShadePlaced = 100;
+
+        }
+            
+    }
+
+    public void NumShadePlacedUpdate()
+    {
+        numShadePlaced += 1;
+    }
+
+    public void OpenInteraction3()
+    {
+        CongestionObjectsWest.SetActive(false);
+    }
+
+
+
+    
+
+}
