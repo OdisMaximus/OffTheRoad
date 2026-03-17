d483325c48cb5691e64b0c85ec6ee41581f20c54
SmoochiDaGucci <jpere257@uic.edu>
2026-03-16 21:27:35 -0500

    Add paintbrush pickup, car models

---
+++ Assets/ProjectAssets/Scripts/PaintbrushPickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PaintbrushPickup : MonoBehaviour
+{
+    public GameObject wandObject;
+    private bool collected = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (collected) return;
+
+        GreeneryGun gun = other.GetComponentInChildren<GreeneryGun>();
+        if (gun == null)
+            gun = other.GetComponentInParent<GreeneryGun>();
+
+        if (gun != null || other.CompareTag("Player"))
+        {
+            collected = true;
+            
+            if (wandObject != null)
+            {
+                wandObject.GetComponent<PaintBrush>().enabled = true;
+                Debug.Log("Paintbrush picked up!");
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
